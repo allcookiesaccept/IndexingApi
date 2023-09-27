8295fef4b7d44d222e2d291961ed2b7129566c06
allcookiesaccept <106693326+allcookiesaccept@users.noreply.github.com>
2023-09-27 15:54:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from manager import IndexingManager
+
+
+def main(urls_csv_file, json_keys_folder):
+    with open(urls_csv_file, "r") as file:
+        urls = file.read().splitlines()
+
+    manager = IndexingManager(urls[:5], json_keys_folder)
+    manager.index_urls()
+
+
+if __name__ == "__main__":
+    urls_csv_file = "urls.csv"
+    json_keys_folder = "json_keys"
+
+    main(urls_csv_file, json_keys_folder)
